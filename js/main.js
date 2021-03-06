8e120c0b6907a838caf6a31dbfd938abedb8e173
Alevtina <alevti89@mail.ru>
2021-03-07 01:06:50 +0300

    переработка на ООП

--- js/main.js
+++ js/main.js
@@ -1,24 +1,78 @@
-const products = [
+class ProductList {
+    constructor (container = ".products"){
+        this.container = container;
+        this.goods = [];
+        this._fetchProducts();
+    }
+    _fetchProducts(){
+        this.goods = [
+            {id: 1, title: 'Notebook', price: 2000},
+            {id: 2, title: 'Mouse', price: 20},
+            {id: 3, title: 'Keyboard', price: 200},
+            {id: 4, title: 'Gamepad', price: 50},
+        ]
+    }
+
+    render(){
+        const block = document.querySelector(this.container);
+        for(let product of this.goods){
+            const  productObj = new ProductItem(product);
+            //block.innerHTML += productObj.render();
+            block.insertAdjacentHTML("beforeend", productObj.render());
+        }
+    }
+
+    summa(){
+        let mas = this.goods;
+        let sum = 0;
+        mas.forEach(function (item){
+            sum += item.price;
+        })
+        alert(sum);
+    }
+
+}
+
+class ProductItem {
+    constructor (product, img = 'https://www.ibisegypttours.com/images/accomadation/blogpost-placeholder-100x100.png'){
+        this.title = product.title;
+        this.price = product.price;
+        this.id = product.id;
+        this.img = img
+    }
+
+    render(){
+        return `<div class="product-item">
+                <img src="${this.img}">
+                <h3 class="product-item-heading">${this.title}</h3>
+                <p class="product-item-price">${this.price}</p>
+                <button class="buy-btn">Купить</button>
+            </div>`
+    }
+}
+
+let list = new ProductList();
+list.render();
+list.summa();
+
+/* const products = [
     {id: 1, title: 'Notebook', price: 2000},
     {id: 2, title: 'Mouse', price: 20},
     {id: 3, title: 'Keyboard', price: 200},
     {id: 4, title: 'Gamepad', price: 50},
 ];
 
-const renderProduct = (title, price) => {
+const renderProduct = (product, img = 'https://www.ibisegypttours.com/images/accomadation/blogpost-placeholder-100x100.png') => {
     return `<div class="product-item">
-                <img src="./img/brains.jpg" alt="" width=100 >
-                <h3 class="product-item-heading">${title}</h3>
-                <p class="product-item-price">${price}</p>
+                <img src="${img}">
+                <h3 class="product-item-heading">${product.title}</h3>
+                <p class="product-item-price">${product.price}</p>
                 <button class="buy-btn">Купить</button>
             </div>`
 };
 
 const renderPage = list => {
-    const productsList = list.map(item => renderProduct(item.title, item.price));
-    console.log(productsList);
-    const str = productsList.join(' ');
-    document.querySelector('.products').innerHTML = str;
+    document.querySelector('.products').innerHTML = list.map(item => renderProduct(item)).join('');
 } 
 
-renderPage(products);
+renderPage(products); */
